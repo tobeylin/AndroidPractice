e257b23ad12a7111d5c9c39fb6334bf82b12022d
Tobey Lin <tobeylin@kkbox.com>
2020-03-10 14:41:52 +0800

    Upgrade LeakCanary from 1.5 to 2.2.

--- app/src/main/java/sample/app/tobeylin/androidpractice/MainApplication.java
+++ app/src/main/java/sample/app/tobeylin/androidpractice/MainApplication.java
@@ -2,36 +2,13 @@ package sample.app.tobeylin.androidpractice;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-import com.squareup.leakcanary.RefWatcher;
-
 public class MainApplication extends Application {
-
-    private RefWatcher refWatcher;
-
-    public static RefWatcher getRefWatcher(Application application) {
-        return ((MainApplication) application).getRefWatcher();
-    }
-
-    public RefWatcher getRefWatcher() {
-        return refWatcher;
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        initLeakCanary();
 //        initDBFlow();
-    }
-
-    private void initLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        refWatcher = LeakCanary.install(this);
     }
 
 //    private void initDBFlow() {
