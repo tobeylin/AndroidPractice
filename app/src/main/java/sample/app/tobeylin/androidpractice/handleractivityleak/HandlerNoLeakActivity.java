b76a66257ebde16b740c02d25b6e5d783378dde4
Tobey Lin <tobeylin@kkbox.com>
2020-03-10 15:19:21 +0800

    Fix the warning in the sample code of handler leak.

--- app/src/main/java/sample/app/tobeylin/androidpractice/handleractivityleak/HandlerNoLeakActivity.java
+++ app/src/main/java/sample/app/tobeylin/androidpractice/handleractivityleak/HandlerNoLeakActivity.java
@@ -1,9 +1,8 @@
 package sample.app.tobeylin.androidpractice.handleractivityleak;
 
-import android.annotation.SuppressLint;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,14 +11,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.ref.WeakReference;
+
 import sample.app.tobeylin.androidpractice.R;
 
-public class HandlerLeakActivity extends AppCompatActivity implements View.OnClickListener {
+public class HandlerNoLeakActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String TAG = HandlerLeakActivity.class.getSimpleName();
+    private static final String TAG = HandlerNoLeakActivity.class.getSimpleName();
 
     private TextView countTextView;
-    private LeakHandler leakHandler = new LeakHandler();
+    private LeakHandler leakHandler;
     private int count = 0;
 
     @Override
@@ -27,8 +28,9 @@ public class HandlerLeakActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handler_leak);
 
+        leakHandler = new LeakHandler(this);
         countTextView = findViewById(R.id.handlerLeakActivity_countText);
-        countTextView.setText(R.string.handler_leak_title);
+        countTextView.setText(R.string.handler_no_leak_title);
         Button plusButton = findViewById(R.id.handlerLeakActivity_plusButton);
         plusButton.setOnClickListener(this);
     }
@@ -48,15 +50,31 @@ public class HandlerLeakActivity extends AppCompatActivity implements View.OnCli
         finish();
     }
 
-    @SuppressLint("HandlerLeak")
-    private class LeakHandler extends Handler {
+    private static class LeakHandler extends Handler {
 
         static final int LONG_TASK = 1;
         static final int UPDATE_UI = 2;
 
+        private WeakReference<HandlerNoLeakActivity> activityRef;
+//        private boolean isCancelled = false;
+
+        LeakHandler(HandlerNoLeakActivity activity) {
+            activityRef = new WeakReference<>(activity);
+        }
+
+//        public void cancel() {
+//            isCancelled = true;
+//            leakHandler = null;
+//        }
+
         @Override
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
+
+//            if (isCancelled) {
+//                return;
+//            }
+
             switch (msg.what) {
                 case LONG_TASK:
                     new Thread(new Runnable() {
@@ -72,7 +90,7 @@ public class HandlerLeakActivity extends AppCompatActivity implements View.OnCli
                     }).start();
                     break;
                 case UPDATE_UI:
-                    countTextView.setText(getResources().getString(R.string.handler_leak_count_text, count));
+                    activityRef.get().countTextView.setText(activityRef.get().getResources().getString(R.string.handler_leak_count_text, activityRef.get().count));
                     break;
             }
         }
